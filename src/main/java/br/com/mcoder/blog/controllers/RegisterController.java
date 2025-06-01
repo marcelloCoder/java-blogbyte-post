ac936df2d1e6f40690a6b7c8075b7f6af8ba3d5f
marcelloCoder <deoliveiral767@gmail.com>
2025-06-01 15:56:16 -0300

    register page working

--- src/main/java/br/com/mcoder/blog/controllers/RegisterController.java
+++ src/main/java/br/com/mcoder/blog/controllers/RegisterController.java
@@ -1,13 +1,31 @@
 package br.com.mcoder.blog.controllers;
 
+import br.com.mcoder.blog.models.Account;
+import br.com.mcoder.blog.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
 
+    @Autowired
+    private AccountService accountService;
+
     @GetMapping("/register")
-    public String getRegisterPage(){
+    public String getRegisterPage(Model model){
+        Account account = new Account();
+        model.addAttribute("account", account);
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerNewSUer(@ModelAttribute Account account){
+        accountService.save(account);
+
+        return "redirect:/";
     }
 }
