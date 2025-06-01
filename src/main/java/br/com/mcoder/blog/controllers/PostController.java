57f5835ad5c1de75aa1258f58cbc357ac6132c2b
marcelloCoder <deoliveiral767@gmail.com>
2025-06-01 16:24:54 -0300

    spring security add

--- src/main/java/br/com/mcoder/blog/controllers/PostController.java
+++ src/main/java/br/com/mcoder/blog/controllers/PostController.java
@@ -1,12 +1,16 @@
 package br.com.mcoder.blog.controllers;
 
+import br.com.mcoder.blog.models.Account;
 import br.com.mcoder.blog.models.Post;
+import br.com.mcoder.blog.services.AccountService;
 import br.com.mcoder.blog.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
@@ -15,6 +19,9 @@ public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @Autowired
+    private AccountService accountService;
 
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable Long id, Model model){
@@ -27,4 +34,24 @@ public class PostController {
             return "404";
         }
     }
+
+    @GetMapping("/posts/new")
+    public String createNewPost(Model model){
+        Optional<Account> optionalAccount = accountService.findByEmail("user.user@email.com");
+        if (optionalAccount.isPresent()){
+            Post post = new Post();
+            post.setAccount(optionalAccount.get());
+            model.addAttribute("post", post);
+            return "post_new";
+        }else {
+            return "404";
+        }
+    }
+
+    @PostMapping("/posts/new")
+    public String saveNewPost(@ModelAttribute Post post){
+        postService.save(post);
+        return "redirect:/posts/" + post.getId();
+    }
+
 }
